fc2dce609895715e6e238b6006fafab0784b4018
l4auti44 <110310425+l4auti44@users.noreply.github.com>
2024-10-31 20:47:56 -0300

    SoundSystem added
    basic sound system

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundManager
+{
+    public enum Sound
+    {
+        Alarm1,
+        Alarm2,
+        Repair,
+        Speaking,
+        Death,
+        Impact
+    }
+
+    public static void PlaySound(Sound sound)
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(GetAudioClip(sound));
+    }
+
+    private static AudioClip GetAudioClip(Sound sound)
+    {
+        foreach(GameAssets.SoundAudioClip soundAudioClip in GameAssets.i.soundAudioClipArray)
+        {
+            if (soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;
+            }
+        }
+
+        Debug.LogError("Sound " + sound + " not found!");
+        return null;
+    }
+}
