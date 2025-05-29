eb0484e4b1538dd68a5b8c92ebc96be33eefae50
l4auti44 <110310425+l4auti44@users.noreply.github.com>
2025-05-29 20:13:24 -0300

    add score text component and update score display logic

---
+++ Assets/Scripts/AsteroidScene/UpdateScoreText.cs
@@ -0,0 +1,25 @@
+
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class UpdateScoreText : MonoBehaviour
+{
+    private TMP_Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        StartCoroutine("UpdateTextCoroutine");
+    }
+
+    IEnumerator UpdateTextCoroutine()
+    {
+        while (true)
+        {
+            scoreText.text = ScoreManager.GetScore().ToString("D12");
+            yield return new WaitForSeconds(0.5f);
+        }
+       
+    }
+}
